691b2d5e94ec1acae9e0ea37baca71b0a0a95172
nzCharlie <charles.jiangbl@gmail.com>
2013-05-21 13:32:47 +1200

    remove karma coverage for now

--- jstest/config/karma.conf.js
+++ jstest/config/karma.conf.js
@@ -2,27 +2,27 @@ frameworks = ["jasmine"];
 
 plugins = [
    'karma-jasmine',
-   'karma-coverage',
+//   'karma-coverage',
    'karma-phantomjs-launcher'
  ];
 
 basePath = '../';
 
 files = [
-  'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.6/angular.min.js',
-  'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.6/angular-resource.min.js',
-  'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+  'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.6/angular.js',
+  'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.6/angular-resource.js',
+  'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js',
   'test/lib/angular/angular-mocks.js',
   '../src/main/webapp/scripts/**/*.js',
   'test/unit/**/*.js'
 ];
 
-preprocessors = {
-  '../src/main/webapp/scripts/*.js': 'coverage'
-};
+//preprocessors = {
+//  '../src/main/webapp/scripts/*.js': 'coverage'
+//};
 
 autoWatch = true;
 
 browsers = ['PhantomJS'];
 
-reporters = ['coverage'];
+//reporters = ['coverage'];
