be7211a985a0a98af484cf521e756aee05e776a4
nzCharlie <charles.jiangbl@gmail.com>
2013-05-23 23:15:32 +1200

    added tests for Bookmark service

--- jstest/test/unit/servicesSpec.js
+++ jstest/test/unit/servicesSpec.js
@@ -5,4 +5,43 @@
 describe('service', function() {
   beforeEach(module('bookmarksServices'));
 
+  describe("Bookmark", function(){
+	  var $httpBackend, BookmarkResource;
+	  
+	  beforeEach(inject(function(_$httpBackend_, Bookmark){
+		  $httpBackend = _$httpBackend_;
+		  BookmarkResource = Bookmark;
+	  }));
+	  
+	  afterEach(function() {
+		  $httpBackend.verifyNoOutstandingExpectation();
+		  $httpBackend.verifyNoOutstandingRequest();
+	  });
+	  
+	  it('should be able to list bookmarks', function(){
+		  $httpBackend.expectGET('/rest/bookmarks/list').respond([{
+			  name: 'test',
+			  url: 'www.test.com',
+			  description: 'this is a test'
+		  }]);
+		  
+		  var bookmarks = BookmarkResource.query({});
+		  $httpBackend.flush();
+		  
+		  expect(bookmarks.length).toBe(1);
+		  expect(bookmarks[0].name).toBe('test');
+		  expect(bookmarks[0].url).toBe('www.test.com');
+		  expect(bookmarks[0].description).toBe('this is a test');
+
+	  });
+	  
+	  it('should be able to create bookmark', function(){
+		  var bookmark = {name:'test',url: 'www.test.com',description: 'this is a test'};
+		  var bookmarkJson = '{"name":"test","url":"www.test.com","description":"this is a test"}';
+		  $httpBackend.expect('PUT', '/rest/bookmarks', bookmarkJson).respond(200);
+		  
+		  BookmarkResource.create(bookmark);
+		  $httpBackend.flush();
+	  });
+  });
 });
