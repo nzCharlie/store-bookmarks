e0a7b537d26ef37c94b7a8f643ae51b0c56eabbd
nzCharlie <charles.jiangbl@gmail.com>
2013-05-24 11:35:39 +1200

    update tests for Bookmark service

--- jstest/test/unit/servicesSpec.js
+++ jstest/test/unit/servicesSpec.js
@@ -6,11 +6,18 @@ describe('service', function() {
   beforeEach(module('bookmarksServices'));
 
   describe("Bookmark", function(){
-	  var $httpBackend, BookmarkResource;
+	  var $httpBackend, BookmarkResource, bookmark;
 	  
 	  beforeEach(inject(function(_$httpBackend_, Bookmark){
 		  $httpBackend = _$httpBackend_;
 		  BookmarkResource = Bookmark;
+      
+      bookmark = {
+        id:   1,
+			  name: 'test',
+			  url: 'www.test.com',
+			  description: 'this is a test'
+		  };
 	  }));
 	  
 	  afterEach(function() {
@@ -19,16 +26,13 @@ describe('service', function() {
 	  });
 	  
 	  it('should be able to list bookmarks', function(){
-		  $httpBackend.expectGET('/rest/bookmarks/list').respond([{
-			  name: 'test',
-			  url: 'www.test.com',
-			  description: 'this is a test'
-		  }]);
+		  $httpBackend.expectGET('/rest/bookmarks/list').respond([bookmark]);
 		  
 		  var bookmarks = BookmarkResource.query({});
 		  $httpBackend.flush();
 		  
 		  expect(bookmarks.length).toBe(1);
+      expect(bookmarks[0].id).toBe(1);
 		  expect(bookmarks[0].name).toBe('test');
 		  expect(bookmarks[0].url).toBe('www.test.com');
 		  expect(bookmarks[0].description).toBe('this is a test');
@@ -36,12 +40,46 @@ describe('service', function() {
 	  });
 	  
 	  it('should be able to create bookmark', function(){
-		  var bookmark = {name:'test',url: 'www.test.com',description: 'this is a test'};
-		  var bookmarkJson = '{"name":"test","url":"www.test.com","description":"this is a test"}';
-		  $httpBackend.expect('PUT', '/rest/bookmarks', bookmarkJson).respond(200);
+		  $httpBackend.expect('PUT', '/rest/bookmarks', angular.toJson(bookmark)).respond(204);
 		  
 		  BookmarkResource.create(bookmark);
 		  $httpBackend.flush();
+	  });
+    
+    it('should be able to get bookmark', function(){
+		  $httpBackend.expect('GET', '/rest/bookmarks/1').respond(bookmark);
+		  
+      var actual = BookmarkResource.get({bookmarkId: bookmark.id});
+		  $httpBackend.flush();
+      
+      expect(actual.id).toBe(1);
+		  expect(actual.name).toBe('test');
+		  expect(actual.url).toBe('www.test.com');
+		  expect(actual.description).toBe('this is a test');
+	  });
+    
+    it('should be able to delete bookmark', function(){
+		  $httpBackend.expect('GET', '/rest/bookmarks/1').respond(bookmark);
+		  
+      var aBookmark = BookmarkResource.get({bookmarkId: 1});
+		  $httpBackend.flush();
+      
+      $httpBackend.expect('DELETE', '/rest/bookmarks/1').respond(204);
+      aBookmark.$delete({bookmarkId: aBookmark.id});
+      $httpBackend.flush();
+	  });
+    
+    it('should be able to save bookmark', function(){
+		  $httpBackend.expect('GET', '/rest/bookmarks/1').respond(bookmark);
+		  
+      var aBookmark = BookmarkResource.get({bookmarkId: 1});
+		  $httpBackend.flush();
+
+      aBookmark.description = 'this';
+      $httpBackend.expect('POST', '/rest/bookmarks/1', angular.toJson(aBookmark)).respond(200);
+
+      aBookmark.$save({});
+      $httpBackend.flush();
 	  });
   });
 });
